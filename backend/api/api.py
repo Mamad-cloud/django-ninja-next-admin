331ddbbe9f4969642cf9f54e636eb075a6ae6ff1
Fatahi Muhamad <fatahimohamad877@gmail.com>
2025-01-20 11:18:28 +0330

    added some comments

--- backend/api/api.py
+++ backend/api/api.py
@@ -2,12 +2,15 @@ from ninja import NinjaAPI
 from .models import Item
 from .schemas import ItemSchema
 
+# instantiating our ninja api class instance 
 api = NinjaAPI()
 
+# returns all items 
 @api.get('/items', response=list[ItemSchema])
 def list_items(request):
     return Item.objects.all()
 
+# creates an item
 @api.post('/items', response=ItemSchema)
 def create_item(request, item: ItemSchema):
     return Item.objects.create(**item.dict())
